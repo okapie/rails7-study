4f038f41db3199ee010dbffae8730ccca2ea913c
okapie <tatigreuze@yahoo.co.jp>
2022-02-13 08:00:13 +0900

    Add new files generated by the following command:   - rails g scaffold Group name:string

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_05_012027) do
+ActiveRecord::Schema.define(version: 2022_02_12_225910) do
+
+  create_table "groups", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
