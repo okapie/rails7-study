0afd231c58b6e9ee0d38e984afc7af1e0b76edfd
okapie <tatigreuze@yahoo.co.jp>
2022-03-19 11:49:11 +0900

    Value object is set to Favorite model.

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,16 @@
+class ToyMaker
+  attr_accessor :toy_maker, :store_name
+  def initialize(toy_maker, store_name)
+    @toy_maker, @store_name = toy_maker, store_name
+  end
+end
+
 class Favorite < ApplicationRecord
   belongs_to :member
   belongs_to :toy, optional: true
+
+  composed_of :toy_maker, mapping: [%w(toy_maker_name toy_maker_name), %w(store_name store_name)]
+  composed_of :store_toy_maker,
+    class_name: 'ToyMaker',
+    mapping: [%w(store_maker_name toy_maker_name), %w(store_name store_name)]
 end
